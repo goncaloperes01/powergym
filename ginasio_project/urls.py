d3da96733b674e0afd2322746d2994cec76dfba0
goncaloperes01 <goncaloperes02@gmail.com>
2026-05-31 23:14:00 +0100

    favicon updated

--- ginasio_project/urls.py
+++ ginasio_project/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico', permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('ginasio.urls')), # ou o nome que deste à tua app
 ]
